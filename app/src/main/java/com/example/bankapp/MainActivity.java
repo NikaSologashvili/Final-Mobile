4f8c1f8fada9f4d58b2e645bd2acb1d99c14c726
Nika Sologashvili <nika.sologashvili05@gmail.com>
2025-06-25 21:09:32 +0400

    Final Push

---
+++ app/src/main/java/com/example/bankapp/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.bankapp;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        if (savedInstanceState == null) {
+            replaceFragment(new HomeFragment());
+        }
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navigation_home) {
+                    replaceFragment(new HomeFragment());
+                    return true;
+                } else if (itemId == R.id.navigation_contacts) {
+                    replaceFragment(new ContactsFragment());
+                    return true;
+                } else if (itemId == R.id.navigation_about) {
+                    replaceFragment(new AboutFragment());
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+}
